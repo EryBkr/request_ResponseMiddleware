089130eee54369a6b8556f0dd9ec90a2e5ea3753
Eray Bakır <info@eraybakir.com>
2022-09-17 21:57:15 +0300

    Project was started

---
+++ RequestResponseMiddleware.Library/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿//C# 10 -- Using tanımlamalarını tek bir yerde tanımladım
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Text;
+global using System.Threading.Tasks;
