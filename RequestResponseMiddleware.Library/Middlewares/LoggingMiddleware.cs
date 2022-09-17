e200adcecd43c432932f7ac9211224dbeaf62c37
Eray Bakır <info@eraybakir.com>
2022-09-17 22:01:43 +0300

    Initilaize Version 2

---
+++ RequestResponseMiddleware.Library/Middlewares/LoggingMiddleware.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Net.Http;
+
+namespace RequestResponseMiddleware.Library.Middlewares
+{
+    class LoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public async Task Invoke(HttpContent context)
+        {
+            
+        }
+    }
+}
